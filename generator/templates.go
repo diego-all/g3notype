f42b3c19e1bdc8827b8d9f2c519d97ebcbe8096b
Diego-All <dposadallano@hotmail.com>
2024-07-01 22:24:41 -0500

    readTemplate function that reads the value from estructura

--- generator/templates.go
+++ generator/templates.go
@@ -2,31 +2,32 @@ package generator
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
 )
 
-// "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/handlers.txt",
-
 // Estructura de archivos y carpetas
 var estructura = map[string]string{
 	"cmd/api/handlers.go":             "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/handlers.txt",
-	"cmd/api/handlers-{{.Entity}}.go": "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/handlers-{{.Entity}}.txt",
-	"cmd/api/main.go":                 "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/main.txt",
-	"cmd/api/routes.go":               "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/routes.txt",
-	"cmd/api/util.go":                 "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/util.txt",
-	"database/connection.go":          "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/connection.txt",
-	"database/up.sql":                 "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/up.sql",
-	"data.sqlite":                     "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/data.sqlite",
-	"golang-CRUD-{{.Entity}}-API.postman_collection.json": "/base-templates",
-	"go.mod":                        "/base-templates",
-	"go.sum":                        "/base-templates",
+	"cmd/api/handlers-{{.Entity}}.go": "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/handlers-entity.txt",
+	//"cmd/api/handlers-{{.Entity}}.go": "/base-templates/cmd/api/handlers-{{.Entity}}.txt",
+	"cmd/api/main.go":        "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/main.txt",
+	"cmd/api/routes.go":      "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/routes.txt",
+	"cmd/api/util.go":        "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/cmd/api/util.txt",
+	"database/connection.go": "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/connection.txt",
+	"database/up.sql":        "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/up.sql",
+	"data.sqlite":            "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/database/data.sqlite",
+	//"golang-CRUD-{{.Entity}}-API.postman_collection.json": "/base-templates",
+	"go.mod":                        "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/go.mod",
+	"go.sum":                        "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/go.sum",
 	"internal/models.go":            "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/internal/models.txt",
 	"internal/{{.EntityPlural}}.go": "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/internal/Entities.txt",
-	"product_classDiagram.png":      "/base-templates",
-	"README.md":                     "/base-templates",
+	//"product_classDiagram.png":      "/base-templates",
+	"README.md": "/home/diegoall/MAESTRIA_ING/CLI/run-from-gh/base-templates/README.md",
 }
 
 // Datos para las plantillas
@@ -54,21 +55,26 @@ func createFolderStructure(appName string, class string) {
 	}
 
 	// Crear la estructura de archivos y carpetas
-	for pathTemplate, content := range estructura {
+	for projectFile, templatePath := range estructura {
 		// Reemplazar los valores en el path
-		path := strings.Replace(pathTemplate, "{{.Entity}}", class, -1)
+		fmt.Println("Path y Content es: ", projectFile, templatePath)
+
+		//readTemplate(content)
+		path := strings.Replace(projectFile, "{{.Entity}}", class, -1)
 		path = strings.Replace(path, "{{.EntityPlural}}", entityPlural, -1)
 		fullPath := filepath.Join(appName, path)
 
 		// Crear las carpetas necesarias
 		dir := filepath.Dir(fullPath)
-		if err := os.MkdirAll(dir, 0755); err != nil {
+		if err := os.MkdirAll(dir, 0777); err != nil {
+			//if err := os.MkdirAll(dir, 0755); err != nil {
 			fmt.Println("Error al crear la carpeta:", err)
 			continue
 		}
 
 		// Crear el archivo
 		file, err := os.Create(fullPath)
+		fmt.Println("Creando archivo: ", file, fullPath)
 		if err != nil {
 			fmt.Println("Error al crear el archivo:", err)
 			continue
@@ -76,8 +82,9 @@ func createFolderStructure(appName string, class string) {
 		defer file.Close()
 
 		// Si hay contenido de plantilla, procesarlo
-		if content != "" {
-			tmpl, err := template.New("fileContent").Parse(content)
+		if templatePath != "" {
+			fmt.Println("entro al if del content")
+			tmpl, err := template.New("fileContent").Parse(templatePath)
 			if err != nil {
 				fmt.Println("Error al parsear la plantilla:", err)
 				continue
@@ -89,4 +96,17 @@ func createFolderStructure(appName string, class string) {
 		}
 	}
 	fmt.Println("Estructura de archivos y carpetas generada con éxito en la carpeta", appName)
+}
+
+func readTemplate(ruta string) {
+	// leer el arreglo de bytes del archivo
+	datosComoBytes, err := ioutil.ReadFile(ruta)
+	fmt.Println("Leyendo template ...")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convertir el arreglo a string
+	datosComoString := string(datosComoBytes)
+	// imprimir el string
+	fmt.Println(datosComoString)
 }
