8c8c00d875351f08c7a8ca3c708e11c95125a550
Diego-All <dposadallano@hotmail.com>
2024-07-08 00:06:29 -0500

    cleaning map types for handlers

--- generator/preTemplates.go
+++ generator/preTemplates.go
@@ -8,12 +8,12 @@ import (
 // Los que no son Req o Res van para la DB
 
 var TypesVars = map[string]string{
-	"handlers-typeEntityRequest":  "",
-	"handlers-typeEntityResponse": "",
-	"handlers-createEntityModels": "",
-	"handlers-readEntResponse":    "",
-	"handlers-updateEntityModels": "",
-	"handlers":                    "",
+	"handlers-typeEntityRequest":     "",
+	"handlers-typeEntityResponse":    "",
+	"handlers-varCreateEntityModels": "",
+	"handlers-varGetEntResponse":     "",
+	"handlers-varUpdateEntityModels": "",
+	"handlers":                       "",
 }
 
 // quizas sea generar Tipos o algo asi, todas las estructuras que dependen de la metadata de clases (atributos)
@@ -31,62 +31,74 @@ func generateClassTags(class string, classMetadata map[string]string) map[string
 	longitud := len(classMetadata)
 	fmt.Println("longitud del map es:", longitud)
 
-	var aux string
+	var auxReqRes string
+
 	var auxCreateEntModels string //Se uso para Update, solo se adicionan 2 campos
-	var auxReadEntResponse string
-	var tagsTypes []string
+	var auxGetEntResponse string
+
+	var reqResTypes []string
 	var createEntModels []string
-	var readEntResponse []string
+	var getEntResponse []string
 	var multilineAux string
-	var multiline string
+
 	var multilineAuxCEntModels string
 	var multilineAuxREntResponse string
-	var multilineCEntModels string
-	var multilineUEntModels string
-	var multilineREntResponse string
+
+	var handlers_typeEntityRequest string
+	var handlers_typeEntityResponse string
+	var handlers_varCreateEntityModels string
+	var handlers_varGetEntResponse string
+	var handlers_varUpdateEntityModels string
 
 	for attribute, value := range classMetadata {
 
-		fmt.Printf("Clave: %s, Valor: %s\n", attribute, value)
+		//fmt.Printf("Clave: %s, Valor: %s\n", attribute, value)
 		fmt.Println((strings.Title(strings.ToLower(attribute))))
 		//capitalized := cases.Title(language.English).String(strings.ToLower(attribute)) // Requiere utilizar golang.org/x/text/cases (al parecer no es estandar)
 		//fmt.Println("CAPITALIZED", capitalized) // Requiere utilizar golang.org/x/text/cases (al parecer no es estandar)
-		fmt.Println("alternativa nativa: ", strings.ToUpper(string(attribute[0]))+string(attribute[1:])) // toco esto para no usar mas dependencias.
+		fmt.Println("Capitalize alternativa nativa: ", strings.ToUpper(string(attribute[0]))+string(attribute[1:])) // toco esto para no usar mas dependencias.
 
-		aux = attribute + "\t" + value + "\t" + "`json:\"" + attribute + "\"`"
-		auxCreateEntModels = attribute + ":\t" + "{{.Entity}}Req." + attribute + ","
-		auxReadEntResponse = attribute + ":\t" + "{{.Entity}}." + attribute + ","
+		//auxReqRes = attribute + "\t" + value + "\t" + "`json:\"" + attribute + "\"`"
+		auxReqRes = strings.ToUpper(string(attribute[0])) + string(attribute[1:]) + "\t" + value + "\t" + "`json:\"" + attribute + "\"`"
+		auxCreateEntModels = strings.ToUpper(string(attribute[0])) + string(attribute[1:]) + ":\t" + "{{.Entity}}Req." + attribute + ","
+		auxGetEntResponse = attribute + ":\t" + "{{.Entity}}." + attribute + ","
 
-		fmt.Println("AUX", aux) // \t
+		//fmt.Println("auxReqRes", auxReqRes)
 		fmt.Println("AUXCREATEENTMODELS", auxCreateEntModels)
-		fmt.Println("AUXREADENTRESPONSE", auxReadEntResponse)
+		fmt.Println("auxGetEntResponse", auxGetEntResponse)
 
-		tagsTypes = append(tagsTypes, aux)
+		//Append de cada una de los atributos leidos
+		reqResTypes = append(reqResTypes, auxReqRes)
 		createEntModels = append(createEntModels, auxCreateEntModels)
-		readEntResponse = append(readEntResponse, auxReadEntResponse)
+		getEntResponse = append(getEntResponse, auxGetEntResponse)
 
 	}
 	fmt.Println("\n")
-	fmt.Println("Array de tags: ", tagsTypes)
+	fmt.Println("Array de reqResTypes: ", reqResTypes)
 	fmt.Println("Array de createEntModels: ", createEntModels)
-	fmt.Println("Array de readEntResponse: ", readEntResponse)
+	fmt.Println("Array de getEntResponse: ", getEntResponse)
 
 	fmt.Println("\n")
 
-	for i, j := range tagsTypes {
+	// Se verticalizan
+	for i, j := range reqResTypes {
 
 		fmt.Println("Valor de i", i, "Valor de j", j)
 		//multilineAux = j +"\n"
-		multilineAux = multilineAux + tagsTypes[i] + "\n"
+		multilineAux = multilineAux + reqResTypes[i] + "\n"
 	}
 
 	fmt.Println("multilineAux: \n ", multilineAux)
 	fmt.Println("\n")
 
 	//Tambien puede servir para el {{.Entity}}Response
-	multiline = "type {{.Entity}}Request struct {" + "\n" + multilineAux + "}"
+	handlers_typeEntityRequest = "type {{.Entity}}Request struct {" + "\n" + multilineAux + "}"
 	fmt.Println("\n")
-	fmt.Println("multilineFinal: \n ", multiline)
+	fmt.Println("handlers_typeEntityRequest: \n ", handlers_typeEntityRequest)
+
+	handlers_typeEntityResponse = "type {{.Entity}}Response struct {" + "\n" + multilineAux + "}"
+	fmt.Println("\n")
+	fmt.Println("handlers_typeEntityResponse: \n ", handlers_typeEntityResponse)
 
 	// Para createEntModels
 	for i, j := range createEntModels {
@@ -98,30 +110,39 @@ func generateClassTags(class string, classMetadata map[string]string) map[string
 	fmt.Println("multilineAuxCEntModels: \n ", multilineAuxCEntModels)
 	fmt.Println("\n")
 
-	//para create
-	multilineCEntModels = "var {{.Entity}} = models.{{.Entity}}{" + "\n" + multilineAuxCEntModels + "}"
+	//para create handlers_varCreateEntityModels
+	handlers_varCreateEntityModels = "var {{.Entity}} = models.{{.Entity}}{" + "\n" + multilineAuxCEntModels + "}"
 	fmt.Println("\n")
-	fmt.Println("var CreateEntityModels: \n ", multilineCEntModels)
+	fmt.Println("handlers_varCreateEntityModels: \n ", handlers_varCreateEntityModels)
 
-	// Para update
-	multilineUEntModels = "var {{.Entity}} = models.{{.Entity}}{" + "\n" + multilineAuxCEntModels + "UpdatedAt:   time.Now()," + "\n" + "Id:          productID," + "\n" + "}"
+	// Para update handlers_varGetEntResponse
+	handlers_varUpdateEntityModels = "var {{.Entity}} = models.{{.Entity}}{" + "\n" + multilineAuxCEntModels + "UpdatedAt:   time.Now()," + "\n" + "Id:          productID," + "\n" + "}"
 	fmt.Println("\n")
-	fmt.Println("var UpdateEntityModels: \n ", multilineUEntModels)
+	fmt.Println("handlers_varUpdateEntityModels: \n ", handlers_varUpdateEntityModels)
 
 	// EN construccion
 
-	for i, j := range readEntResponse {
+	for i, j := range getEntResponse {
 		fmt.Println("Valor de i", i, "Valor de j", j)
-		multilineAuxREntResponse = multilineAuxREntResponse + readEntResponse[i] + "\n"
+		multilineAuxREntResponse = multilineAuxREntResponse + getEntResponse[i] + "\n"
 
 	}
 
 	fmt.Println("multilineAuxREntResponse: \n ", multilineAuxREntResponse)
 	fmt.Println("\n")
 
-	multilineREntResponse = "var {{.Entity}}Response = {{.Entity}}Response{\n" + multilineAuxREntResponse + "}"
+	handlers_varGetEntResponse = "var {{.Entity}}Response = {{.Entity}}Response{\n" + multilineAuxREntResponse + "}"
 	fmt.Println("\n")
-	fmt.Println("var ReadEntityResponse: \n ", multilineREntResponse)
+	fmt.Println("handlers_varGetEntResponse: \n ", handlers_varGetEntResponse)
+
+	// Generated Types and Vars
+	TypesVars["handlers-typeEntityRequest"] = handlers_typeEntityRequest
+	TypesVars["handlers-typeEntityResponse"] = handlers_typeEntityResponse
+	TypesVars["handlers-varCreateEntityModels"] = handlers_varCreateEntityModels
+	TypesVars["handlers-varGetEntResponse"] = handlers_varGetEntResponse
+	TypesVars["handlers-varUpdateEntityModels"] = handlers_varUpdateEntityModels
+
+	fmt.Println("TIPO FINAL: ", TypesVars)
 
 	return TypesVars
 	//return multiline // antes retornaba el primer type EntityRequest
